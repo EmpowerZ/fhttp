660e7d63ce2a7d38a449a3e07481fde0882d0f94
Andrew Onishchuk <aonishuk@hortonworks.com>
2023-05-30 11:44:14 +0200

    Don't add user-agent header

--- request.go
+++ request.go
@@ -617,11 +617,6 @@ func (r *Request) write(w io.Writer, usingProxy bool, extraHeaders Header, waitF
 		r.Header.Set("Host", host)
 	}
 
-	// if user agent field is not present, add it
-	if uaCap, uaLow := r.Header["User-Agent"], r.Header["user-agent"]; uaCap == nil && uaLow == nil {
-		r.Header.Set("User-Agent", "Go-http-client/1.1")
-	}
-
 	// Process Body,ContentLength,Close,Trailer
 	tw, err := newTransferWriter(r)
 	if err != nil {
